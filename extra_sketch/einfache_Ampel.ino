04c406a13046424ffc94e332fa9c6a7112f5ae6b
Neikinikless <67499407+Neikinikless@users.noreply.github.com>
2021-02-03 11:55:51 +0100

    Einfacher Ampel Sketch Added
    Extra Sketch: Einfache Ampel Hinzugefügt

---
+++ extra_sketch/einfache_Ampel.ino
@@ -0,0 +1,45 @@
+
+//Extra Aufgaben und Erweiterungen
+
+
+void setup() {
+
+ pinMode(7, OUTPUT); //roteLED
+  pinMode(9, OUTPUT); //orangeLED
+   pinMode(8, OUTPUT); //grüneLED
+
+ /*
+   
+
+   So auch möglich:
+
+   int roteLED=7;
+
+   pinMode(roteLED, OUTPUT);
+  
+ 
+  */
+ 
+}
+
+
+void loop() {
+
+digitalWrite(7, HIGH);
+delay(5000);
+digitalWrite(9, HIGH);
+delay(3000);
+digitalWrite(7, LOW);
+digitalWrite(9, LOW);
+digitalWrite(8, HIGH);
+delay(3000);
+digitalWrite(9, HIGH);
+delay(2000);
+digitalWrite(8, LOW);
+digitalWrite(7, HIGH);
+delay(2000);
+digitalWrite(9, LOW);
+
+
+
+}
